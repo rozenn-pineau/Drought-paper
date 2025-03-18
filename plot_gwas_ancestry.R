0460389bb177c43d052c39f34440520906b67314
rozenn-pineau <141448081+rozenn-pineau@users.noreply.github.com>
2025-03-18 14:31:35 -0500

    Update plot_gwas_ancestry.R

--- plot_gwas_ancestry.R
+++ plot_gwas_ancestry.R
@@ -15,28 +15,12 @@ qlog10 <- function(p.values) {
   return(-log10(theoretical))
 }
  
-
-#read me !
-#this script is testing a lot of different file. Make sure you run the lines you are interested in !
-
+#this script is for the current set of filters that we are using - might be updated if we chose other combinations of filters. 
+#It also does not include all previous analyses that led ud to the choice of these filters.
 
 # ----------------------------------------------------------------------------- #
 # read in regression results files
 # ----------------------------------------------------------------------------- #
-setwd("/Users/rozenn/Library/CloudStorage/GoogleDrive-rozennpineau@uchicago.edu/My Drive/Work/9.Science/1.DroughtProject/1.analyses/data/8.ancestry_hmm/1.results/gwas/manual/cutoff08")
-list.files()
-#cutoff 0.8
-lmm_manual_cov <- read.table("logistic_covariate_regression_results_cutoff_08.txt", header= T , sep = "\t") 
-
-setwd("/Users/rozenn/Library/CloudStorage/GoogleDrive-rozennpineau@uchicago.edu/My Drive/Work/9.Science/1.DroughtProject/1.analyses/data/8.ancestry_hmm/1.results/gwas/gemma/cutoff08")
-list.files()
-#cutoff 0.8 and NAs tolerance to 10% (default is 5%)
-lmm_gemma_geno <- read.table("geno_corrected_gemma_gwas.assoc.txt", header= T, sep = "\t")
-lmm_gemma_anc <- read.table("ancestry_corrected_gemma_gwas.assoc.txt", header= T, sep = "\t") 
-
-#modify rs col to get chrom position information
-lmm_gemma_geno[c("chrom", "pos")] <- as.numeric(str_split_fixed(lmm_gemma_geno$rs, ':', 2))
-lmm_gemma_anc[c("chrom", "pos")] <- as.numeric(str_split_fixed(lmm_gemma_anc$rs, ':', 2))
 
 #new runs with -miss 0.2 and cutoff at 0.9
 setwd("/Users/rozenn/Library/CloudStorage/GoogleDrive-rozennpineau@uchicago.edu/My Drive/Work/9.Science/1.DroughtProject/1.analyses/figures/8.ancestry_hmm/gwas/gemma_cutoff09_miss02")
@@ -51,8 +35,6 @@ lmm_gemma_anc[c("chrom", "pos")] <- as.numeric(str_split_fixed(lmm_gemma_anc$rs,
 # ----------------------------------------------------------------------------- #
 # correct p-values
 # ----------------------------------------------------------------------------- #
-
-df_vec <- c("lmm_manual_cov", "lmm_gemma_geno", "lmm_gemma_anc")
 df_vec <- c("lmm_gemma", "lmm_gemma_geno", "lmm_gemma_anc")
 
 for (d in 1:length(df_vec)) {
@@ -82,12 +64,6 @@ for (d in 1:length(df_vec)) {
 # QQPlots
 # ----------------------------------------------------------------------------- #
 
-# QQplot with base 10 - lrt test for corrected manual gwas
-ggplot(lmm_manual_cov, aes(x = qlogp, y = -log10(pval))) + 
-  geom_path() + ylim(0,10) + xlim(0,10) + 
-  geom_abline(intercept = 0, slope = 1)
-# to test if there is enrichment in low p-values compared to the null, which are uniformly distributed under the null
-
 # QQplot with base 10 - lrt test for ancestry corrected gemma gwas
 ggplot(lmm_gemma, aes(x = qlogp, y = -log10(p_wald))) + 
   geom_path() + ylim(0,10) + xlim(0,10) + 
@@ -109,7 +85,7 @@ ggplot(lmm_gemma_geno, aes(x = qlogp, y = -log10(p_wald))) +
 #inflation factor 
 
 # Number of tests (GWAS SNPs)
-sorted_pvals<-sort(lmm_gemma_anc$p_wald)
+sorted_pvals <- sort(lmm_gemma_anc$p_wald)
 n <- length(sorted_pvals)
 
 # Expected p-values under the null hypothesis
@@ -120,7 +96,7 @@ observed_chi2 <- qchisq(1 - sorted_pvals, df=1)  # Chi-squared test statistic fo
 observed_lambda <- mean(observed_chi2) / qchisq(0.5, df=1)  # Mean observed chi-squared statistic divided by the expected (median)
 
 # Output the inflation factor (lambda)
-observed_lambda #2.601994
+observed_lambda #2.356066
 # Adjust p-values using genomic control
 lmm_gemma_anc$inflation_p <- pmin(lmm_gemma_anc$p_wald * observed_lambda, 1)  # p-values can't exceed 1
 
@@ -133,6 +109,18 @@ hist(-log10(lmm_gemma_anc$inflation_p))
 lmm_gemma_anc$FDR<-p.adjust(lmm_gemma_anc$inflation_p,method = "fdr")
 lmm_gemma_anc$bon<-p.adjust(lmm_gemma_anc$inflation_p,method = "bonferroni")
 lmm_gemma_anc$qlogp <- qlog10(lmm_gemma_anc$inflation_p)
+
+# ----------------------------------------------------------------------------- #
+# export association file with inflated corrected p-values
+# ----------------------------------------------------------------------------- #
+write.table(lmm_gemma_anc, "ancestry_corrected_inflated_gemma_gwas.assoc.txt", sep = "\t", col.names = T, row.names = F, quote = F)
+
+#export chromosome and positions that are significant FDR sites to filter the herbarium vcf
+sites <- lmm_gemma_anc$FDR < 0.05 #893
+export <- data.frame(chrom = lmm_gemma_anc$chrom[sites],
+                     pos1 = lmm_gemma_anc$pos[sites]-1,
+                     pos2 = lmm_gemma_anc$pos[sites])
+write.table(export, "FDR_non_clumped_significant_sites.txt", sep = "\t", col.names = T, row.names = F, quote = F)
 
 # ----------------------------------------------------------------------------- #
 # quick look at potentially significant SNPs
@@ -154,6 +142,7 @@ for (d in 1:length(df_vec)) {
 
 }
 
+quick_stats
 
 # ----------------------------------------------------------------------------- #
 # QQPlots
@@ -170,71 +159,27 @@ ggplot(lmm_gemma_anc, aes(x = qlogp, y = -log10(inflation_p))) +
 # Manhattan plot
 # ----------------------------------------------------------------------------- #
 
-#logistic models
-setwd("/Users/rozenn/Library/CloudStorage/GoogleDrive-rozennpineau@uchicago.edu/My Drive/Work/9.Science/1.DroughtProject/1.analyses/figures/8.ancestry_hmm/gwas")
-pdf(file="manual_logistic_covariate_cutoff08.pdf", 
-    bg = "transparent", width=8, height=4, family = "Times New Roman")
-
-idx <- which(lmm_manual_cov$bon == max(lmm_manual_cov$bon[which(lmm_manual_cov$bon < 0.08)]))
-idx2 <- which(lmm_manual_cov$FDR == max(lmm_manual_cov$FDR[which(lmm_manual_cov$FDR < 0.08)]))
-p1<- lmm_manual_cov %>%
-  ggplot(aes(pos/1000000,-log10(pval))) +
-  geom_point(alpha=.5, col = "steelblue") +
-  facet_grid(. ~ chrom, scales = "free_x", space = "free_x") +
-  theme_classic() +
-  xlab("Position in Mb") +
-  ylim(0,10) +
-  theme(axis.text.x = element_text(angle = 90)) +
-geom_hline(yintercept = -log10(lmm_manual_cov$pval[idx]),lty="dashed",lwd=.5) +
-geom_hline(yintercept = -log10(lmm_manual_cov$pval[idx2]),lty="dashed",lwd=.5, col = "gray") 
-
-
-p1 
-
-dev.off()
-
-
-
-#GEMMA linear regression
-pdf(file="/Users/rozenn/Library/CloudStorage/GoogleDrive-rozennpineau@uchicago.edu/My Drive/Work/9.Science/1.DroughtProject/1.analyses/figures/8.ancestry_hmm/gwas/gemma_cutoff09_miss02/gemma_cutoff09_miss02.pdf", 
-    bg = "transparent", width=8, height=4, family = "Times New Roman")
-
-idx <- which(lmm_gemma$bon == max(lmm_gemma$bon[which(lmm_gemma$bon < 0.05)]))
-idx2 <- which(lmm_gemma$FDR == max(lmm_gemma$FDR[which(lmm_gemma$FDR < 0.05)]))
-p1<- lmm_gemma %>%
-  ggplot(aes(pos/1000000,-log10(p_wald))) +
-  geom_point(alpha=.5, col = "steelblue") +
-  facet_grid(. ~ chrom, scales = "free_x", space = "free_x") +
-  #facet_wrap(~ chrom, scales = "free_x")+
-  theme_classic() +
-  xlab("Position in Mb") +
-  #ylim(0,10) +
-  theme(axis.text.x = element_text(angle = 90)) +
-  geom_hline(yintercept = -log10(lmm_gemma$p_wald[idx]),lty="dashed",lwd=.5) +
-  geom_hline(yintercept = -log10(lmm_gemma$p_wald[idx2]),lty="dashed",lwd=.5, col = "gray") 
-
-
-p1 
-
-dev.off()
-
 #GEMMA linear regression with correction
-pdf(file="/Users/rozenn/Library/CloudStorage/GoogleDrive-rozennpineau@uchicago.edu/My Drive/Work/9.Science/1.DroughtProject/1.analyses/figures/8.ancestry_hmm/gwas/gemma_cutoff09_miss02/gemma_anc_cutoff09_miss02.pdf", 
-    bg = "transparent", width=8, height=4, family = "Times New Roman")
+pdf(file="/Users/rozenn/Library/CloudStorage/GoogleDrive-rozennpineau@uchicago.edu/My Drive/Work/9.Science/1.DroughtProject/0.Writing/1.Paper/1.Figures/Figure3/gwas_on_ancestry.pdf", 
+    bg = "transparent", width=10, height=3, family = "Times New Roman")
 
 idx <- which(lmm_gemma_anc$bon == max(lmm_gemma_anc$bon[which(lmm_gemma_anc$bon < 0.05)]))
 idx2 <- which(lmm_gemma_anc$FDR == max(lmm_gemma_anc$FDR[which(lmm_gemma_anc$FDR < 0.05)]))
+
+#colors
+lmm_gemma_anc$cols <- "gray"
+lmm_gemma_anc$cols[lmm_gemma_anc$FDR < 0.05] <- "red" #893
 p1<- lmm_gemma_anc %>%
   ggplot(aes(pos/1000000,-log10(p_wald))) +
-  geom_point(alpha=.5, col = "steelblue") +
+  geom_point(alpha=.5, col = lmm_gemma_anc$cols) +
   facet_grid(. ~ chrom, scales = "free_x", space = "free_x") +
   #facet_wrap(~ chrom, scales = "free_x")+
   theme_classic() +
   xlab("Position in Mb") +
   ylim(0,10) +
   theme(axis.text.x = element_text(angle = 90)) +
-  geom_hline(yintercept = -log10(lmm_gemma_anc$p_wald[idx]),lty="dashed",lwd=.5) +
-  geom_hline(yintercept = -log10(lmm_gemma_anc$p_wald[idx2]),lty="dashed",lwd=.5, col = "gray") 
+  geom_hline(yintercept = -log10(lmm_gemma_anc$p_wald[idx]),lty="dashed",lwd=.5, col = "#929292") +
+  geom_hline(yintercept = -log10(lmm_gemma_anc$p_wald[idx2]),lty="dashed",lwd=.5, col = "#929292") 
 
 
 p1 
